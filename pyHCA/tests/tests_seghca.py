40e353839cbb3f23606854b299f7970fe0a26a9d
T-B-F <T-B-F@users.noreply.github.com>
2019-03-04 13:49:05 +0100

    fix test

--- pyHCA/tests/tests_seghca.py
+++ pyHCA/tests/tests_seghca.py
@@ -13,7 +13,7 @@ class TestSegHCA(unittest.TestCase):
         self.check_sequence = "NGRHTGFGRTCCDKGADHLKGEGHCCITLAKRGYFPCEPWCTLLFALNMFNMQNMMRQQFSDDHNNMGRLCQQTTHRFPFNSDNKEEYIWLYKVQRLGAW"
         #self.check_domains = {0: [26, 70, 0.0004118383429020378],
         #                      1: [87, 100, np.nan]}
-        self.check_domains = {0: [6, 100, 0.002042687753916028]}
+        self.check_domains = {0: [6, 100, 0.0022]}
         #self.check_clusters = {0: [ 6,  7,  np.array([1])],
         #                       1: [18, 19,  np.array([1])],
         #                       2: [26, 29,  np.array([1, 0, 1])],
